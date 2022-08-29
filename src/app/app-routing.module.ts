c6e67463bdc3a76b6cdb47b1e9df4d70aecbde26
Franco_Osores <franco_osores@hotmail.com>
2022-08-28 22:43:16 -0300

    Epic page setup, no requests yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './modules/presentation/routes/home/home.component';
+import { MyEpicsComponent } from './modules/presentation/routes/my-epics/my-epics.component';
 import { MyProjectsComponent } from './modules/presentation/routes/my-projects/my-projects.component';
 
 const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
-	{ path: 'my-projects', component:MyProjectsComponent}
+	{ path: 'my-projects', component: MyProjectsComponent },
+	{ path: 'my-projects/:id', component: MyEpicsComponent },
 ];
 
 @NgModule({

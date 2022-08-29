339ed76862f454c3c303e6d990cddd907dc3986e
Franco_Osores <franco_osores@hotmail.com>
2022-08-29 15:06:57 -0300

    Fix: minor style changes

--- src/app/modules/presentation/routes/my-tasks/my-tasks.component.ts
+++ src/app/modules/presentation/routes/my-tasks/my-tasks.component.ts
@@ -9,7 +9,19 @@ import { DialogFormComponent } from '../../components/dialog-form/dialog-form.co
 	styleUrls: ['./my-tasks.component.scss'],
 })
 export class MyTasksComponent implements OnInit {
-	tasks: Tasks[] = [];
+	tasks: Tasks[] = [
+		{
+			done: false,
+			_id: '630cf4375d621400168e0bf1',
+			id: 143,
+			name: 'Task 1',
+			description: 'This is task #999',
+			story: '620192ba5d34515ecc3adafa',
+			created: '2022-08-29T17:15:35.396Z',
+			due: '2022-08-29T17:15:35.396Z',
+			__v: 0,
+		},
+	];
 	constructor(public dialog: MatDialog) {}
 
 	openDialog() {

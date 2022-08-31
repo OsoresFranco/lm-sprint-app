0c29afd276abe2415a33a5a2f7936a2cf821c943
Franco_Osores <franco_osores@hotmail.com>
2022-08-31 09:41:54 -0300

    Added Validators to login form

--- src/app/modules/presentation/routes/login/login.component.ts
+++ src/app/modules/presentation/routes/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/modules/api-rest/services/auth.service';
 
@@ -19,8 +19,8 @@ export class LoginComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.login = this.fb.group({
-			username: '',
-			password: '',
+			username: ['', [Validators.required, Validators.minLength(4)]],
+			password: ['', [Validators.required, Validators.minLength(4)]],
 		});
 	}
 

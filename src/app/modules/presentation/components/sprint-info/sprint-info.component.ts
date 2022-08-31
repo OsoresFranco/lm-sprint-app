164e4d97ef79c26f5037a940cfc0ae962a4d09c8
Franco_Osores <franco_osores@hotmail.com>
2022-08-31 13:11:52 -0300

    Feat: finished home layout

--- src/app/modules/presentation/components/sprint-info/sprint-info.component.ts
+++ src/app/modules/presentation/components/sprint-info/sprint-info.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sprint-info',
-  templateUrl: './sprint-info.component.html',
-  styleUrls: ['./sprint-info.component.scss']
+	selector: 'app-sprint-info',
+	templateUrl: './sprint-info.component.html',
+	styleUrls: ['./sprint-info.component.scss'],
 })
 export class SprintInfoComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
